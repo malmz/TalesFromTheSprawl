3af7dae46b4732f014733855960b2e4f0f64bd33
Carl Malmz <callem217@gmail.com>
2023-10-02 20:26:56 +0200

    fix path

--- talesbot/logger.py
+++ talesbot/logger.py
@@ -2,17 +2,20 @@ import logging
 
 cmd_logger = None
 
+
 def setup_command_logger():
     global cmd_logger
-    logpath = "logs/all_commands.log"
-    cmd_logger = logging.getLogger('log')
+    logpath = "config/logs/all_commands.log"
+    cmd_logger = logging.getLogger("log")
     cmd_logger.setLevel(logging.INFO)
     ch = logging.FileHandler(logpath)
-    ch.setFormatter(logging.Formatter('%(message)s'))
+    ch.setFormatter(logging.Formatter("%(message)s"))
     cmd_logger.addHandler(ch)
+
 
 def log_command(author_id, player_name, channel, content):
     cmd_logger.info("%s : %s : %s : %s" % (author_id, player_name, channel, content))
+
 
 def failed_to_log():
     cmd_logger.warn("Failed to log command (something went wrong)")
