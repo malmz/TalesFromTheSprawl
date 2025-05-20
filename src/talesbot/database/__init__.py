d686a9482b9837a029ae414728e83b26f4c38224
Carl Malmgren <callem217@gmail.com>
2025-05-20 16:23:34 +0200

    use async db engine

--- src/talesbot/database/__init__.py
+++ src/talesbot/database/__init__.py
@@ -1,32 +1,20 @@
-import sqlite3
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass
 
-from sqlalchemy import Engine, create_engine, event
-from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass, sessionmaker
+from talesbot.config import config
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./config/data.sqlite"
+engine = create_async_engine(config.SQLALCHEMY_DATABASE_URI)
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-
-SessionM = sessionmaker(engine)
+SessionM = async_sessionmaker(engine, expire_on_commit=False)
 
 
-class Base(MappedAsDataclass, DeclarativeBase):
+class Base(AsyncAttrs, MappedAsDataclass, DeclarativeBase):
     pass
 
 
-def create_tables():
-    Base.metadata.create_all(engine)
+async def create_tables():
+    # Import models so they are registerd
+    from . import models  # noqa: F401
 
-
-@event.listens_for(Engine, "connect")
-def set_sqlite_pragma(dbapi_connection: sqlite3.Connection, connection_record):
-    cursor = dbapi_connection.cursor()
-    cursor.execute("PRAGMA journal_mode=WAL")
-    cursor.execute("PRAGMA busy_timeout=5000")
-    cursor.execute("PRAGMA synchronous=NORMAL")
-    cursor.execute("PRAGMA cache_size=-20000")
-    cursor.execute("PRAGMA foreign_keys=ON")
-    cursor.execute("PRAGMA temp_store=MEMORY")
-    cursor.close()
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
